7883434130aa1cd8eb220b4328cc2f3f2cf8f9d7
SeverNikolay <124562620+SeverNikolay@users.noreply.github.com>
2024-06-11 11:07:47 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import xml.etree.ElementTree as ET
+
+graph = []   # граф связей машин между собой
+vms = {}     # вспомогательный словарь сопоставлений id машин и их имён
+
+# вспомогательный словарь соспоставлений шейпов и типов машин
+vmtypes = {
+             'mxgraph.cisco19.rect': 'router',
+             'mxgraph.cisco19.workstation': 'workstation',
+             'mxgraph.cisco19.server': 'server',
+             'cloud': 'internet'
+          }
+
+vmtp = [] #Сопоставление устройств и их типов
+
+tree = ET.parse('DE2.drawio')
+root = tree.getroot()[0][0][0]
+
+# формируем словарь vms
+for cell in root:
+    style = cell.attrib.get('style')
+    value = cell.attrib.get('value')
+    ident = cell.attrib.get('id')
+    if isinstance(style, str):
+        part = style.partition('shape=')
+        if part[1] != '':
+            vms[ident] = value
+            #print(part[2].partition(';')[0])
+            
+            #Сопоставление устройств и их типов, запись в файл
+            vmtp.append((vms[ident], vmtypes[part[2].partition(';')[0]]))
+            #with open ('fileType.txt', 'w') as file:
+            #    for item in vmtp:
+            #        dataStr=' : '.join(item)
+            #        file.write(dataStr + '\n')
+
+# строим граф
+for cell in root.findall('mxCell'):
+    for geometry in cell.findall('mxGeometry'):
+        for point in geometry.findall('mxPoint'):
+            if point.attrib.get('as') == 'sourcePoint':
+                graph.append((vms[cell.attrib.get('source')], vms[cell.attrib.get('target')]))
+                break
+
+#Запись данный в файл 
+#with open ('fileGraph.txt', 'w') as file:
+#    for item in graph:
+#        dataStr=' : '.join(item)
+#        print(dataStr)
+#        file.write(dataStr + '\n')
+
+print(graph, '\n')
+print(vmtp)
+
+# nbr - это количество мостов на одном стенде без учета моста vmbr0 в WAN
+counter = 0
+for link in graph:
+    if link[0] == 'Интернет' or link[1] == 'Интернет':
+        counter += 1
+nbr = len(graph) - counter
+
+print(nbr)
